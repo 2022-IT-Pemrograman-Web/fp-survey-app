fc87a69553eba58da4a222a46931be055e85d701
JayantiTA <jayjayanti18@gmail.com>
2022-12-09 09:07:08 +0700

    feat: create login page and register page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,70 @@
 <template>
-  <HelloWorld />
+  <v-card
+    class="d-flex flex-column mx-auto my-16 py-5 px-5"
+    max-width="600"
+    min-height="300"
+  >
+    <v-card-title>
+      <p class="text-h5 text--primary my-1">ques·tion·naire</p>
+      <p class="text-h3 text--primary my-5">Login</p>
+    </v-card-title>
+    <v-card-text>
+      <v-form v-model="v$.$dirty" @submit.prevent="onSubmit">
+        <v-text-field
+          class="my-1"
+          v-model="user.username"
+          :error-messages="v$.user.username.$error"
+          label="Username"
+          required
+        ></v-text-field>
+        <v-text-field
+          class="my-1"
+          v-model="user.password"
+          :error-messages="v$.user.password.$error"
+          label="Password"
+          required
+        ></v-text-field>
+        <v-btn
+          type="submit"
+          color="purple"
+          class="my-3"
+          :disabled="v$.user.$invalid"
+          >Login</v-btn
+        >
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import useVuelidate from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
 
-// Components
-import HelloWorld from "../components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "HomeView",
-
-  components: {
-    HelloWorld,
+export default {
+  setup() {
+    return { v$: useVuelidate() };
   },
-});
+
+  data() {
+    return {
+      user: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+
+  validations() {
+    return {
+      user: {
+        username: {
+          required,
+        },
+        password: {
+          required,
+        },
+      },
+    };
+  },
+};
 </script>
