b548001be9d92cc24610d3f38a061d4f4011bdec
JayantiTA <jayjayanti18@gmail.com>
2022-12-15 10:01:50 +0700

    feat: fix create survey view

--- src/views/CreateSurveyView.vue
+++ src/views/CreateSurveyView.vue
@@ -8,40 +8,89 @@
       <p class="text-h3 text--primary my-5">New Survey</p>
     </v-card-title>
     <v-card-text>
-      <v-form v-model="v$.$dirty" @submit.prevent="addQuestion" lazy-validation>
+      <v-form
+        v-model="v$.$dirty"
+        @submit.prevent="newSurvey()"
+        lazy-validation
+        v-if="survey.title.length === 0 || isEditedTitle"
+      >
         <v-text-field
           class="my-1"
-          v-model="tempQuestion"
-          :error-messages="v$.tempQuestion.$error"
-          label="Question"
-          placeholder="Do you like an apple?"
+          v-model="tempTitle"
+          :error-messages="v$.tempTitle.$error"
+          label="Title"
+          placeholder="This is Survey Form"
           required
         ></v-text-field>
+        <v-textarea
+          class="my-1"
+          v-model="tempDescription"
+          label="Description (optional)"
+          placeholder="Type some descriptions..."
+        ></v-textarea>
         <v-btn
           type="submit"
           color="purple"
-          class="my-3"
-          :disabled="v$.tempQuestion.$invalid"
-          >Add Question</v-btn
+          class="mb-10"
+          :disabled="v$.tempTitle.$invalid"
+          >{{ isEditedTitle ? "Update" : "Create Survey" }}</v-btn
         >
       </v-form>
-      <v-list-item
-        v-for="question in questions"
-        :key="question"
-        class="d-flex flex-row justify-space-between"
-      >
+      <div v-else>
         <v-list-item-content>
-          <v-list-item-title>{{ question }}</v-list-item-title>
+          <v-list-item-title>{{ survey.title }}</v-list-item-title>
+          <v-list-item-subtitle>{{ survey.description }}</v-list-item-subtitle>
         </v-list-item-content>
         <v-list-item-action>
-          <v-btn size="35" icon @click="deleteQuestion(question)">
-            <v-icon>mdi-delete</v-icon>
+          <v-btn size="35" icon @click="editTitle()">
+            <v-icon>mdi-pencil</v-icon>
           </v-btn>
         </v-list-item-action>
-      </v-list-item>
-      <v-btn color="purple" class="my-3" :disabled="questions.length === 0"
-        >Submit Questions</v-btn
-      >
+      </div>
+      <div v-if="survey.title.length > 0">
+        <v-form
+          v-model="v$.$dirty"
+          @submit.prevent="addQuestion"
+          lazy-validation
+        >
+          <v-text-field
+            class="my-1"
+            v-model="tempQuestion"
+            :error-messages="v$.tempQuestion.$error"
+            label="Question"
+            placeholder="Do you like an apple?"
+            required
+          ></v-text-field>
+          <v-btn
+            type="submit"
+            color="purple"
+            class="my-3"
+            :disabled="v$.tempQuestion.$invalid"
+            >Add Question</v-btn
+          >
+        </v-form>
+        <v-list-item
+          v-for="question in survey.questions"
+          :key="question"
+          class="d-flex flex-row justify-space-between"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ question }}</v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn size="35" icon @click="deleteQuestion(question)">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+        <v-btn
+          color="purple"
+          class="my-3"
+          :disabled="survey.questions.length === 0"
+          @click="submitSurvey"
+          >Submit Questions</v-btn
+        >
+      </div>
     </v-card-text>
   </v-card>
 </template>
@@ -56,13 +105,15 @@ export default {
   },
   data() {
     return {
-      user: {
-        username: "",
-        password: "",
-        tes: "",
-      },
+      tempTitle: "",
+      tempDescription: "",
       tempQuestion: "",
-      questions: [],
+      isEditedTitle: false,
+      survey: {
+        title: "",
+        description: "",
+        questions: [],
+      },
     };
   },
   validations() {
@@ -70,18 +121,38 @@ export default {
       tempQuestion: {
         required,
       },
+      tempTitle: {
+        required,
+      },
     };
   },
   methods: {
     addQuestion() {
-      this.questions.push(this.tempQuestion);
+      this.survey.questions.push(this.tempQuestion);
       this.tempQuestion = "";
     },
     deleteQuestion(question) {
-      this.questions = this.questions.filter((q) => q !== question);
+      this.survey.questions = this.survey.questions.filter(
+        (q) => q !== question
+      );
     },
     registerPage() {
       this.$router.push("/register");
+    },
+    editTitle() {
+      this.tempTitle = this.survey.title;
+      this.tempDescription = this.survey.description;
+      this.isEditedTitle = true;
+    },
+    newSurvey() {
+      this.survey.title = this.tempTitle;
+      this.survey.description = this.tempDescription;
+      this.tempTitle = "";
+      this.tempDescription = "";
+      this.isEditedTitle = false;
+    },
+    submitSurvey() {
+      console.log(this.survey);
     },
   },
 };
